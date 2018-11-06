68c1ee1eb9769cb0412f34544832d01dc1282f0f
haeejuut <haeejuut@student.jyu.fi>
2018-11-06 13:30:51 +0200

    nyt  se onnistuu

---
+++ MuseoAR/Assets/Materials/dissolveShader.shader
@@ -0,0 +1,54 @@
+﻿Shader "Custom/SHADER_blog_Dissolve_Texture"
+{
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+
+		_DissolvePercentage("DissolvePercentage", Range(0,1)) = 0.0
+		_ShowTexture("ShowTexture", Range(0,1)) = 0.0
+	}
+	SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+		Cull front
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		half _DissolvePercentage;
+		half _ShowTexture;
+		fixed4 _Color;
+
+		void surf(Input IN, inout SurfaceOutputStandard o)
+		{
+			// Albedo comes from a texture tinted by color
+			half gradient = tex2D(_MainTex, IN.uv_MainTex).r;
+			clip(gradient - _DissolvePercentage);
+
+			fixed4 c = lerp(1, gradient, _ShowTexture) * _Color;
+			o.Albedo = c.rgb;
+
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+	ENDCG
+	}
+		FallBack "Diffuse"
+}
---
+++ MuseoAR/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class SphereScript : MonoBehaviour {
+
+    MeshRenderer rend;
+    GameObject videoSphere, dissolvingSphere;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        rend = transform.GetChild(1).GetComponent<MeshRenderer>();
+        videoSphere = transform.GetChild(0).gameObject;
+        dissolvingSphere = transform.GetChild(1).gameObject;
+        cam = FindObjectOfType<Camera>();
+        // aloitetaan dissolve kun objekti aktiivinen
+        StartCoroutine(startDissolve());
+        videoSphere.GetComponent<VideoPlayer>().Prepare();
+        //videoSphere.transform.GetChild(0).gameObject.SetActive(false);
+
+    }
+
+    public TrackableBehaviour tb;
+    private void Update()
+    {
+        if (tb.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            dissolvingSphere.SetActive(true);            
+            videoSphere.SetActive(true);
+        }
+        if (videoSphere.activeSelf == true)
+        {
+            Quaternion q = cam.transform.rotation;
+            transform.rotation = Quaternion.Euler(q.x, q.y, q.z);
+        }
+    }
+
+    /// <summary>
+    /// Dissolves the material of the meshrenderer 
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator startDissolve()
+    {        
+        if (rend.material.GetFloat("_DissolvePercentage") >= 1)
+            yield break;
+        
+        var dispercentage = rend.material.GetFloat("_DissolvePercentage");
+        rend.material.SetFloat("_DissolvePercentage", dispercentage + .003f);
+        yield return new WaitForSecondsRealtime(.001f);
+        StartCoroutine(startDissolve());
+    }
+}
