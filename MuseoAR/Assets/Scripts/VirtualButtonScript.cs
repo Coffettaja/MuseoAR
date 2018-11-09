0f4b1cdaea9a17a11a8854055c3d749e1232336f
haeejuut <haeejuut@student.jyu.fi>
2018-11-09 13:17:09 +0200

    ar napeista luovuttu

---
+++ MuseoAR/Assets/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class VirtualButtonScript : MonoBehaviour {
+
+    private GameObject tekstiboksi;
+
+    // Use this for initialization
+    void Start()
+    {
+        tekstiboksi = GameObject.Find("testiText");
+    }
+}
