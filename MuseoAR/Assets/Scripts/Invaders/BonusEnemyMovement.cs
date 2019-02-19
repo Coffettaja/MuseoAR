5c280851ca9239786a8c70d38f27ce23c3efbb6e
puupertti <thaapane@student.jyu.fi>
2019-02-19 15:18:31 +0200

    Erotettu vihollisten liikkumislogiikka ja muu logiikka erilleen. Bonusvihollinen räjähtää, mutta räjähdysefekti on väärässä asennossa.

---
+++ MuseoAR/Assets/Scripts/Invaders/BonusEnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusEnemyMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float SinCurveMultiplier;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        moveEnemy();
+	}
+
+    private void moveEnemy()
+    {
+        Vector3 height = Mathf.Sin(Time.time * SinCurveMultiplier) * Vector3.up;
+        Vector3 moveVector = height + Vector3.right;
+        transform.Translate(moveVector * Time.deltaTime * movementSpeed, Space.World);
+    }
+}
---
+++ MuseoAR/Assets/Scripts/Invaders/EnemyScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    
+    public GameObject deathEffect;
+    public int pointsValue;
+    protected InvadersManagerScript _manager;
+    private GameObject imageTarget;    
+
+    private void Awake()
+    {
+        imageTarget = GameObject.Find("ImageTarget");
+        _manager = imageTarget.GetComponent<InvadersManagerScript>();
+    }
+
+    /// <summary>
+    /// Tells the enemy to die. Enemy then informs the manager to add points to score.
+    /// </summary>
+    public void Die()
+    {
+        AddPoints();
+        Explode();
+
+        //Destroy the enemy
+        _manager.RemoveEnemyFromList(gameObject);
+        Destroy(this.gameObject);
+
+        _manager.CheckIfStageCompleted();
+    }
+
+    public void Explode()
+    {
+        //Create and animate the enemy's death effect
+        Transform transform = GetComponent<Transform>();
+        GameObject explosion = Instantiate(deathEffect, transform.position, transform.rotation, imageTarget.transform);
+        ParticleSystem ps = explosion.GetComponent<ParticleSystem>();
+        ps.Play();
+        var em = ps.emission;
+        em.enabled = true;
+
+        //Destroy the Particle System so it doesn't linger on indefinetely
+        Destroy(explosion, 1.0f);
+    }
+
+    public void AddPoints()
+    {
+        _manager.Score += pointsValue;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "GameOverPlane")
+        {
+            _manager.GameOver();
+        }
+    }
+
+    public void Stop()
+    {
+        var moveScript = GetComponent<EnemyMovement>();
+        moveScript.Stop();
+    }
+}
