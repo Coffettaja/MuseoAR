65329a6c0b7ab8dd0995c4567c0717d88a0e425b
Joonas Karttunen <coffettaja@gmail.com>
2019-04-09 20:03:58 +0300

    Hide the UI when taking a selfie.

--- MuseoAR/Assets/Scripts/Selfie/ScreenshotAndShare.cs
+++ MuseoAR/Assets/Scripts/Selfie/ScreenshotAndShare.cs
@@ -9,16 +9,20 @@ using UnityEngine.UI;
 /// and triggers the native share function
 /// </summary>
 public class ScreenshotAndShare : MonoBehaviour {
+  private RectTransform canvas;
+  private Vector3 initScale;
 
-
-	// Use this for initialization
-	void Start () {
+  // Use this for initialization
+  void Start () {
         Button screenshotButton = GetComponent<Button>();
         screenshotButton.onClick.AddListener(StartScreenshotCoroutine);
+    canvas = transform.parent as RectTransform;
+    initScale = canvas.localScale;
 	}
 	
     private void StartScreenshotCoroutine()
     {
+    canvas.localScale = Vector3.zero;
         StartCoroutine("TakeAndShare");
     }
 
@@ -31,20 +35,22 @@ public class ScreenshotAndShare : MonoBehaviour {
         //Texture2D ss = new Texture2D(Screen.width, (int)screenHeight, TextureFormat.RGB24, false);
         //TODO: Hide the UI while takign screenshot
         Texture2D ss = ScreenCapture.CaptureScreenshotAsTexture();
+    canvas.localScale = initScale;
         //ss.ReadPixels(new Rect(0, startHeight, Screen.width, screenHeight), 0, 0);
         //ss.Apply();
 
-        string filePath = Path.Combine(Application.temporaryCachePath, "sharedScore.png");
+    string filePath = Path.Combine(Application.temporaryCachePath, "selfie.png");
         File.WriteAllBytes(filePath, ss.EncodeToPNG());
 
         // To avoid memory leaks
         GameObject.Destroy(ss);
 
         NativeShare nativeShare = new NativeShare();
-        nativeShare.SetSubject("Viiksikuva!");
-        nativeShare.SetText("'mlady!");
+        nativeShare.SetSubject("MuseoAR Selfie!"); // Primarily for email.
+        //nativeShare.SetText("");
         nativeShare.AddFile(filePath, "image/png");
         nativeShare.SetTitle("Score in Sunset Falls");
         nativeShare.Share();
+
     }
 }
