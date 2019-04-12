fbbdfb7b0447e46bbd0a4ca62dcc5e7de0497f61
karjalane <nino.karjalainen@gmail.com>
2019-04-12 15:54:53 +0300

    Invaders tähtäin

--- MuseoAR/Assets/Scripts/Invaders/ShootingScript.cs
+++ MuseoAR/Assets/Scripts/Invaders/ShootingScript.cs
@@ -17,7 +17,75 @@ public class ShootingScript : MonoBehaviour {
     public GameObject aimPlane;
 
     private float nextShoot = 0.0f;
-    
+
+    //Crosshair
+
+    public bool drawCrosshair = true;
+    public Color crosshairColor = Color.white; // Crosshair color
+    public float width = 1; // Crosshair width 
+    public float height = 3; // Crosshair height
+
+    [System.Serializable]
+    public class spreading
+    {
+        public float sSpread = 20; // Adjust for bigger or smaller crosshair
+        public float maxSpread = 40;
+        public float minSpread = 20;
+        public float spreadPerSecond = 30;
+        public float decreasePerSecond = 25;
+    }
+
+    public spreading spread = new spreading();
+
+    Texture2D tex;
+    float newHeight;
+    GUIStyle lineStyle;
+
+    void Awake ()
+    {
+        tex = new Texture2D(1, 1);
+        lineStyle = new GUIStyle();
+        lineStyle.normal.background = tex;
+    }
+
+    void OnGUI ()
+    {
+        Vector2 centerPoint = new Vector2(Screen.width / 2, Screen.width / 3);
+        float screenRatio = Screen.height / 100;
+
+        newHeight = height * screenRatio;
+
+        if (drawCrosshair)
+        {
+            GUI.Box(new Rect(centerPoint.x - (width / 2), centerPoint.y - (newHeight + spread.sSpread), width, newHeight), GUIContent.none, lineStyle);
+            GUI.Box(new Rect(centerPoint.x - (width / 2), (centerPoint.y + spread.sSpread), width, newHeight), GUIContent.none, lineStyle);
+            GUI.Box(new Rect((centerPoint.x + spread.sSpread), (centerPoint.y - (width / 2)), newHeight, width), GUIContent.none, lineStyle);
+            GUI.Box(new Rect(centerPoint.x - (newHeight + spread.sSpread), (centerPoint.y - (width / 2)), newHeight, width), GUIContent.none, lineStyle);
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            spread.sSpread += spread.spreadPerSecond * Time.deltaTime * 4; // Increment the spread
+            shoot();
+        }
+
+        spread.sSpread -= spread.decreasePerSecond * Time.deltaTime; // Decrement the spread
+        spread.sSpread = Mathf.Clamp(spread.sSpread, spread.minSpread, spread.maxSpread);
+    }
+
+    // Apply color to crosshair
+    void SetColor(Texture2D myTexture, Color myColor)
+    {
+        for (int y = 0; y < myTexture.height; y++)
+        {
+            for (int x = 0; x < myTexture.width; x++)
+                myTexture.SetPixel(x, y, myColor);
+            myTexture.Apply();
+        }
+    }
+
+    //end of crosshair
+
     public void Update()
     {
         //Raycasts to aimplane and then aims the projectile spawn to point at the aimpoint
