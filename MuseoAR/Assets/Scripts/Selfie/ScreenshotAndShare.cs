cbfa69462737922b746c5df3a858176f9c0ccf03
Joonas Karttunen <coffettaja@gmail.com>
2019-04-22 19:44:56 +0300

    Basic emailing functionality.

--- MuseoAR/Assets/Scripts/Selfie/ScreenshotAndShare.cs
+++ MuseoAR/Assets/Scripts/Selfie/ScreenshotAndShare.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +16,8 @@ using UnityEngine.UI;
 public class ScreenshotAndShare : MonoBehaviour {
 
   public List<RectTransform> itemsToHide;
-  private Vector3 initScale;
+  public bool publicDevice = false;
+  //private Vector3 initScale;
 
   // Use this for initialization
   void Start () {
@@ -56,21 +62,81 @@ public class ScreenshotAndShare : MonoBehaviour {
         //ss.ReadPixels(new Rect(0, startHeight, Screen.width, screenHeight), 0, 0);
         //ss.Apply();
 
-    string name = string.Format("{0}_{1}_{2}.png", Application.productName, "{0}", System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
-    Debug.Log("Permission result: " + NativeGallery.SaveImageToGallery(ss, Application.productName, name));
+    string name = string.Format("{0}_{1}", Application.productName, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    //Debug.Log("Permission result: " + NativeGallery.SaveImageToGallery(ss, Application.productName, name));
     string filePath = Path.Combine(Application.temporaryCachePath, name + ".png");
       File.WriteAllBytes(filePath, ss.EncodeToPNG());
 
     // To avoid memory leaks
     GameObject.Destroy(ss);
 
-        NativeShare nativeShare = new NativeShare();
-    
-        nativeShare.SetSubject("MuseoAR Selfie!"); // Primarily for email.
-        //nativeShare.SetText("");
-        nativeShare.AddFile(filePath, "image/png");
-        nativeShare.SetTitle("Score in Sunset Falls");
-        nativeShare.Share();
+    if (publicDevice)
+    {
+      PublicShare(filePath);
+    } else
+    {
+      Debug.Log("Permission result: " + NativeGallery.SaveImageToGallery(ss, Application.productName, name));
+      NativeShare nativeShare = new NativeShare();
 
+      nativeShare.SetSubject("MuseoAR Selfie!"); // Primarily for email.
+                                                 //nativeShare.SetText("");
+      nativeShare.AddFile(filePath, "image/png");
+      nativeShare.SetTitle("Score in Sunset Falls");
+      nativeShare.Share();
     }
+  }
+
+  // More or less copied from
+  // https://answers.unity.com/questions/1184875/email-attachment-on-android-ios.html
+  private void SendEmail(string destinationEmail, string filepath)
+  {
+    string FileName = "";
+
+    Debug.Log("In public share");
+
+    if(!File.Exists(filepath)) {
+      Debug.Log(filepath + " doesn't exist.");
+      return;
+    }
+
+    MailMessage mail = new MailMessage();
+
+    mail.From = new MailAddress("museoartestemail@gmail.com");
+    mail.To.Add(destinationEmail);
+    mail.Subject = "MuseoAR Selfie";
+    mail.Body = "Kawaii~";
+
+    Attachment data = new Attachment(filepath, System.Net.Mime.MediaTypeNames.Application.Octet);
+
+    // Add time stamp information for the file.
+    System.Net.Mime.ContentDisposition disposition = data.ContentDisposition;
+    disposition.CreationDate = System.IO.File.GetCreationTime(filepath);
+    disposition.ModificationDate = System.IO.File.GetLastWriteTime(filepath);
+    disposition.ReadDate = System.IO.File.GetLastAccessTime(filepath);
+
+    mail.Attachments.Add(data);
+
+    SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+    smtpServer.Port = 587;
+    smtpServer.Credentials = new System.Net.NetworkCredential("museoartestmail@gmail.com", "z4laz4na") as ICredentialsByHost;
+    smtpServer.EnableSsl = true;
+    ServicePointManager.ServerCertificateValidationCallback =
+        delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+          return true;
+        };
+    try
+    {
+      smtpServer.Send(mail);
+    }
+    catch (Exception e)
+    {
+      Debug.Log(e.GetBaseException());
+    }
+  }
+
+  
+  private void PublicShare(string filepath)
+  {
+    SendEmail("coffettaja@gmail.com", filepath);
+  }
 }
