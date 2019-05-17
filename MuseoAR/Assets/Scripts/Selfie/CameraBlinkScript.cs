45cc92d71b665d2b526e637a2bb5993f6004782e
Joonas Karttunen <coffettaja@gmail.com>
2019-05-17 23:16:42 +0300

    Blink effect when taking a selfie.

---
+++ MuseoAR/Assets/Scripts/Selfie/CameraBlinkScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBlinkScript : MonoBehaviour {
+
+	public void Blink()
+  {
+    gameObject.SetActive(true);
+    StartCoroutine("BlinkEffect");
+  }
+
+  private IEnumerator BlinkEffect()
+  {
+    yield return new WaitForSeconds(.15f);
+    gameObject.SetActive(false);
+  }
+}
