d79380956c3c8041afe8db00bcf64f16f5ae5fd2
Joonas Karttunen <coffettaja@gmail.com>
2019-06-17 21:06:23 +0300

    Back buttons get a bit darker on tap (just to give at least some feedback to the user).

--- MuseoAR/Assets/Scripts/Selfie/SelfieBackButton.cs
+++ MuseoAR/Assets/Scripts/Selfie/SelfieBackButton.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelfieBackButton : MonoBehaviour {
   public void BackToInitScene()
   {
     GameControllerScript.Instance.MarkSceneCompleted("Selfie");
+    gameObject.GetComponent<Button>().interactable = false;
     GameControllerScript.Instance.LoadTopLevelScene();
 
   }
